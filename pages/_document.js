7488224c5ba43109fb121dc80062b4cf5a1f82ac
Cresencio <cresenciocantu@gmail.com>
2022-12-11 13:16:26 -0600

    update metadata

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="PitchIt" key="title"/>
+        <meta property="og:description" content="Craft an impactful 90-second elevator pitch for your startup." key="description"/>
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="./assets/meta.png"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
